2b42b30c922a1d1dece37a349c6c44587c2edbc6
Turtleju <turtleju@gmail.com>
2020-09-02 18:25:24 +0200

    hotfixes

--- app/javascript/components/title.js
+++ app/javascript/components/title.js
@@ -1,35 +1,24 @@
 const initTitle = () => {
-  const textWrapper = document.querySelector('.main-subtitle .letters');
-    textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+      const textWrapper = document.querySelector('.main-subtitle');
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-    anime.timeline({loop: true})
-      .add({
-        targets: '.main-subtitle .line',
-        scaleY: [0,1],
-        opacity: [0.5,1],
-        easing: "easeOutExpo",
-        duration: 700
-      })
-      .add({
-        targets: '.main-subtitle .line',
-        translateX: [0, document.querySelector('.main-subtitle .letters').getBoundingClientRect().width + 10],
-        easing: "easeOutExpo",
-        duration: 900,
-        delay: 100
-      }).add({
-        targets: '.main-subtitle .letter',
-        opacity: [0,1],
-        easing: "easeOutExpo",
-        duration: 800,
-        offset: '-=775',
-        delay: (el, i) => 34 * (i+1)
-      }).add({
-        targets: '.main-subtitle',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-      });
+      anime.timeline({loop: true})
+        .add({
+          targets: '.main-subtitle .letter',
+          translateX: [40,0],
+          translateZ: 0,
+          opacity: [0,1],
+          easing: "easeOutExpo",
+          duration: 1200,
+          delay: (el, i) => 500 + 30 * i
+        }).add({
+          targets: '.main-subtitle .letter',
+          translateX: [0,-30],
+          opacity: [1,0],
+          easing: "easeInExpo",
+          duration: 1100,
+          delay: (el, i) => 100 + 30 * i
+        });
 }
 
 
